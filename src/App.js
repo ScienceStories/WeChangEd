1849ea72422747407f9a08a7d62e92ebb87e777b
Kenneth Seals-Nutt <k.sealsnutt@gmail.com>
2020-02-08 15:12:56 -0800

    FEAT: Adding Stories API Configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import StoriesAPIStory, { StoriesAPICollection } from 'react-stories-api';
+
 import logo from './logo.svg';
 import './App.css';
+import {
+  storiesAPICollectionId, storiesAPIEndpoint, storiesAPIKey
+} from "./constants";
+
+const LandingPage = () => (
+  <StoriesAPICollection
+    apiKey={storiesAPIKey}
+    id={2}
+    endpoint={storiesAPIEndpoint}
+    urlFormatter="/$id"
+  />
+);
+
+const StoryPage = (props) => (
+  <StoriesAPIStory
+    apiKey={storiesAPIKey}
+    id={props.match.params.storyId}
+    collection={storiesAPICollectionId}
+    endpoint={storiesAPIEndpoint}
+  />
+)
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/:storyId" component={StoryPage} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const storiesAPIEndpoint = "https://stories-api-stage.herokuapp.com";
+export const storiesAPIKey = "rf9qXEDE.SqlziAxtY58fTbSyx2MoKz2naXO9UlvE";
+export const storiesAPICollectionId = 2;
