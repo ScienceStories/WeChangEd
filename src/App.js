728bb9c5a3016b844a477e82df23d672c6ff93da
Kenneth Seals-Nutt <k.sealsnutt@gmail.com>
2020-03-08 21:06:47 -0700

    FEAT: Updating Stories API Package and Adding a Footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import React from 'react';
-import { Button, createMuiTheme, MuiThemeProvider, } from '@material-ui/core';
+import {
+  Button, createMuiTheme, Grid, MuiThemeProvider, makeStyles, Typography
+} from '@material-ui/core';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import StoriesAPIStory, { StoriesAPICollection } from 'react-stories-api';
 
-import './App.css';
 import {
-  logo, storiesAPICollectionId, storiesAPIEndpoint, storiesAPIKey, theme,
+  affiliationLogos, logo, projectUrl, storiesAPICollectionId,
+  storiesAPIEndpoint, storiesAPIKey, theme,
 } from "./constants";
 
+
+const useStyles = makeStyles(theme => ({
+  app: {
+    textAlign: "center",
+    height: "100vh",
+    width: "100vw",
+  },
+  footer: {
+    background: theme.palette.background.paper,
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+    boxShadow: theme.shadows[4],
+  },
+  button: {
+    color: theme.palette.background.default,
+  },
+  container: {
+    width: "100%",
+  },
+  footerLogo:{
+    maxHeight: theme.typography.h1.fontSize,
+    maxWidth: "100%",
+  },
+  light:{
+    opacity: .5,
+    padding: theme.spacing(1)
+  }
+}));
+
+
 const LandingPage = () => (
-  <StoriesAPICollection
-    apiKey={storiesAPIKey}
-    id={storiesAPICollectionId}
-    endpoint={storiesAPIEndpoint}
-    urlFormatter="#$id"
-  />
+  <div>
+    <StoriesAPICollection
+      apiKey={storiesAPIKey}
+      id={storiesAPICollectionId}
+      endpoint={storiesAPIEndpoint}
+      urlFormatter="#$id"
+    />
+    <div style={{padding: 100}} />
+    <Footer />
+  </div>
 );
 
 const StoryPage = (props) => (
@@ -31,12 +68,45 @@ const StoryPage = (props) => (
   />
 )
 
+const Footer = (props) => {
+  const classes = useStyles();
 
-function App() {
+  return (
+    <footer className={classes.footer}>
+      <Grid container className={classes.container} justify="center" alignItems="center">
+        <Grid item xs={4}>
+          <Button className={classes.button} href={projectUrl} target="_blank">
+            <img
+              className={classes.footerLogo}
+              alt="WeChangEd Affiliates Logos"
+              src={affiliationLogos}
+            />
+          </Button>
+        </Grid>
+        <Grid item xs={7} className={classes.light}>
+          <Typography variant="caption">
+            &copy; {1900 + new Date().getYear()} , In Collaboration with
+            <Button
+              size="small"
+              color="secondary"
+              href="http://sciencestories.io"
+              target="_blank"
+              >
+              The ScienceStories.io Team
+            </Button>
+          </Typography>
+        </Grid>
+      </Grid>
+    </footer>
+  );
+};
+
+export default function App() {
+  const classes = useStyles();
   return (
     <HashRouter hashType="noslash">
       <MuiThemeProvider theme={createMuiTheme(theme)}>
-        <div className="App">
+        <div className={classes.app}>
           <Switch>
             <Route path="/:storyId" component={StoryPage} />
             <Route path="/" component={LandingPage} />
@@ -45,6 +115,4 @@ function App() {
       </MuiThemeProvider>
     </HashRouter>
   );
-}
-
-export default App;
+};
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
+export const projectUrl = "https://www.wechanged.ugent.be/";
 export const logo = "https://www.wechanged.ugent.be/wp-content/uploads/2015/12/agentsofchange_logo_baseline.png";
-export const storiesAPIEndpoint = "http://stage.stories.k2.services";
+export const affiliationLogos = "https://www.wechanged.ugent.be/wp-content/uploads/2018/09/logos.png";
+export const storiesAPIEndpoint = "https://stories-api-stage.herokuapp.com";
 export const storiesAPIKey = "rf9qXEDE.SqlziAxtY58fTbSyx2MoKz2naXO9UlvE";
 export const storiesAPICollectionId = 2;
 
